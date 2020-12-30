92de2c3ee49c29d85673aaa107893c006a565f7b
rusimka <rusimkad@gmail.com>
2020-12-30 17:02:48 +0100

    added FileModel

---
+++ src/main/java/proba3/demo/model/File.java
@@ -0,0 +1,27 @@
+package proba3.demo.model;
+
+public class File {
+    private int id;
+    private String fileName;
+
+    public File(int id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
